8c156a18051a3bdcf32c2460e347b9c16708bb27
Diego Requena <diego.requena@utec.edu.pe>
2023-08-29 16:56:42 -0500

    prueba.py

--- prueba.py
+++ prueba.py
@@ -8,127 +8,179 @@
 #if __name__=='__main__':
 #    app.run()           #crear un metodo para q se ejecute flask
 
-from flask import Flask, jsonify
+from flask import Flask,request, jsonify
 app = Flask(__name__)
 
-db=[
+lista=[
     {
         "id":1,
         "titulo":"Gintama: THE FINAL",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx114129-RLgSuh6YbeYx.jpg",
         "categoria":["action","comedy","drama","sci-fi"],
-        "rating":"91%",
-        "reviews":"33663 user",
-        "season":"1 hour, 44 mins",
+        "rating":91,
+        "reviews":33663,
+        "season":"Winter 2021",
         "tipo":"movie",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx114129-RLgSuh6YbeYx.jpg",
+        
     },
     {
         "id":2,
         "titulo":"Gintama°",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx20996-kBEGEGdeK1r7.jpg",
         "categoria":["action","comedy","drama","sci-fi"],
-        "rating":"90 %",
-        "reviews":"87963 users",
-        "season":"51 episodes",
+        "rating":90,
+        "reviews":87963,
+        "season":"Spring 2015",
         "tipo":"TV show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx20996-kBEGEGdeK1r7.jpg",
     },
     {
         "id":3,
         "titulo":"Fruits Basket: The Final",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx124194-pWfBqp3GgjOx.jpg",
         "categoria":["comedy","drama","psychological","romance","sci-fi"],
-        "rating":"90%",
-        "reviews":"118020 users",
-        "season":"13 espisodes",
+        "rating":90,
+        "reviews":118020,
+        "season":"Spring 2021",
         "tipo":"TV Show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx124194-pWfBqp3GgjOx.jpg",
     },
     {
         "id":4,
         "titulo":"Hagane no Renkinjutsushi: FULLMETAL ALCHEMIST",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx5114-KJTQz9AIm6Wk.jpg",
         "categoria":["action","adventure","drama","fantasy"],
-        "rating":"90%",
-        "reviews":"494524 users",
-        "season":"64 episodes",
+        "rating":90,
+        "reviews":494524,
+        "season":"Spring 2009",
         "tipo":"TV Show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx5114-KJTQz9AIm6Wk.jpg",
     },
     {
         "id":5,
         "titulo":"Kaguya-sama wa Kokurasetai: Ultra Romantic",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx125367-bl5vGalMH2cC.png",
         "categoria":["comedy","psychological","romance","sci-fi"],
-        "rating":"90%",
-        "reviews":"176923 users",
-        "season":"13 episodes",
+        "rating":90,
+        "reviews":176923,
+        "season":"Spring 2022",
         "tipo":"TV Show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx125367-bl5vGalMH2cC.png",
     },
     {
         "id":6,
         "titulo":"Shingeki no Kyojin 3 Part 2",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx104578-LaZYFkmhinfB.jpg",
         "categoria":["action","drama","fantasy","mystery"],
-        "rating":"90%",
-        "reviews":"411090 users",
-        "season":"10 episodes",
+        "rating":90,
+        "reviews":411090,
+        "season":"Spring 2019",
         "tipo":"TV Show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx104578-LaZYFkmhinfB.jpg",
     },
     {
         "id":7,
         "titulo":"3-gatsu no Lion 2",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx98478-dF3mpSKiZkQu.jpg",
         "categoria":["drama","sci-fi"],
-        "rating":"89%",
-        "reviews":"102391 users",
-        "season":"22 episodios",
+        "rating":89,
+        "reviews":102391,
+        "season":"Fall 2017",
         "tipo":"TV Show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx98478-dF3mpSKiZkQu.jpg",
     },
     {
         "id":8,
         "titulo":"HUNTERHUNTER (2011)",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx11061-sIpBprNRfzCe.png",
         "categoria":["action","adventure","fantasy"],
-        "rating":"89%",
-        "reviews":"575350 users",
-        "season":"148 episodes",
+        "rating":89,
+        "reviews":575350,
+        "season":"2011 - 2014",
         "tipo":"TV Show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx11061-sIpBprNRfzCe.png",
     },
     {
         "id":9,
         "titulo":"Owarimonogatari (Ge)",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx21745-vHwC1VKoL6zf.png",
         "categoria":["comedy","mystery","psychological","romance","supernatural"],
-        "rating":"89%",
-        "reviews":"92090 users",
-        "season":"7 episodes",
+        "rating":89,
+        "reviews":92090,
+        "season":"Summer 2017",
         "tipo":"TV Show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx21745-vHwC1VKoL6zf.png",
     },
     {
         "id":10,
         "titulo":"Steins;Gate",
+        "poster":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx9253-7pdcVzQSkKxT.jpg",
         "categoria":["drama","psychological","sci-fi","thriller"],
-        "rating":"89%",
-        "reviews":"410650 users",
-        "season":"24 episodes",
+        "rating":89,
+        "reviews":410650,
+        "season":"Spring 2011",
         "tipo":"TV Show",
-        "img":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/bx9253-7pdcVzQSkKxT.jpg",
     }
 ]
 
 @app.route("/")    
 def hello():             
-    return "Inicioooo"
+    return "Pagina de Iniciooo"
 
-@app.route('/anime', methods=["GET"])
+@app.route("/anime", methods=["GET"])
 def anime():
-    return jsonify(db)
+    return jsonify(lista)
+
+
+@app.route("/anime", methods=["POST"])
+def crear_anime():
+    nuevo_anime = {
+        "id": len(lista) + 1,  
+        "titulo": request.json.get("titulo"),
+        "poser": request.json.get("poster"),
+        "categoria":request.json.get("categoria"),
+        "rating":request.json.get("rating"),
+        "reviews":request.json.get("reviews"),
+        "season":request.json.get("season"),
+        "tipo":request.json.get("tipo"),
+    }
+    lista.append(nuevo_anime)
+    return jsonify(nuevo_anime), 201
+
 
 @app.route("/anime/<int:id>", methods=["GET"])
-def animeId(id):
-    anime_id = next((anime for anime in db if anime['id'] == int(id)), None)
+def buscar_id(id):
+    anime_id = next((anime for anime in lista if anime['id'] == int(id)), None)
     if anime_id:
         return jsonify(anime_id)
     else:
-        return "Anime no encontrado", 404
+        return "No se encontro el anime", 404
+
+
+@app.route("/anime/<int:id>", methods=["DELETE"])
+def delete(id):
+    anime_id = next((anime for anime in lista if anime['id'] == int(id)), None)
+    if anime_id:
+        lista.remove(anime_id)
+        return jsonify({"message": "Anime eliminado"}), 200
+    else:
+        return "No se encontro el anime", 404
+
+
+@app.route("/anime/<int:id>", methods=["PUT"])
+def put_total(id):
+    anime_id = next((anime for anime in lista if anime['id'] == int(id)), None)
+    if anime_id:
+        anime_id.update(request.json)
+        return jsonify(anime_id), 200
+    else:
+        return "No se encontro el anime", 404
+
+
+@app.route("/anime/<int:id>", methods=["PATCH"])
+def patch_parcial(id):
+    anime_id = next((anime for anime in lista if anime['id'] == int(id)), None)
+    if anime_id:
+        data = request.json
+        for key, value in data.items():
+            if key in anime_id:
+                anime_id[key] = value  # Actualiza el campo específico con el valor proporcionado
+        return jsonify(anime_id), 200
+    else:
+        return "No se encontro el anime", 404
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
